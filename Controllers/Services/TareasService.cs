bb74fe8006fcded0a75d2ed59d0787252f327a4d
Brayan Rojas Lemus <bmrojasl@ufpso.edu.co>
2023-04-28 17:02:51 -0500

    *Se agrega servicio de categoris y tareas.

---
+++ Controllers/Services/TareasService.cs
@@ -0,0 +1,60 @@
+using System.Runtime.InteropServices;
+using webapi.Models;
+
+namespace  webapi.Services;
+
+public class TareaService : ITareaServices
+{
+    TareasContext context;
+
+    public TareaService(TareasContext dbcontext)
+    {
+        context = dbcontext;
+    }
+
+    public IEnumerable<Tarea> Get()
+    {
+        return context.Tareas;
+    }
+
+    public async Task Save(Tarea tarea)
+{
+    context.Add(tarea);
+    await context.SaveChangesAsync();
+}
+
+    public async Task Update(Guid id,Tarea tarea)
+    {
+        var tareaActual = context.Tareas.Find(id);
+
+        if(tareaActual != null)
+        {
+            tareaActual.Titulo = tarea.Titulo;
+            tarea.Descripcion =  tarea.Descripcion;
+            tarea.Resumen = tarea.Resumen;
+
+            await context.SaveChangesAsync();
+        }
+    }
+
+    public async Task Delete(Guid id)
+    {
+        var tareaActual = context.Tareas.Find(id);
+
+        if(tareaActual != null)
+        {
+            context.Remove(tareaActual);
+            await context.SaveChangesAsync();
+        }
+    }   
+
+
+}
+
+public interface ITareaServices
+{
+   IEnumerable<Tarea> Get();
+    Task Save(Tarea tarea);
+    Task Update(Guid id,Tarea tarea);
+    Task Delete(Guid id);
+}
